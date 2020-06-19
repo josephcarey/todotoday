7464a1b0caa49ae0cd6814d6c70eb4c2739dc477
Joseph Carey <josephcareycomposer@gmail.com>
2020-06-19 13:56:20 -0500

    add basic mapping of todos on front end

--- src/App/App.js
+++ src/App/App.js
@@ -1,18 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-function App() {
+// eslint-disable-next-line max-lines-per-function
+const App = () => {
+  const [todos, setTodos] = useState([{ id: 66 }]);
+
+  const getData = () => {
+    axios.get("api/todo").then((response) => {
+      setTodos(response.data);
+    });
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <div>
       <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
+        <h1>TodoToday</h1>
       </header>
+      <h2>To Dos:</h2>
+      <ul>
+        {todos.map((todo) => {
+          return (
+            <li key={todo.id}>
+              {todo.id}. {todo.text}
+            </li>
+          );
+        })}
+      </ul>
+      {JSON.stringify(todos)}
     </div>
   );
-}
+};
 
 export default App;
