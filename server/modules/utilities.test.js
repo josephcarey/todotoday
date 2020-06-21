04f2ee80aeec456adf2cc660a7e8f02a6541763f
Joseph Carey <josephcareycomposer@gmail.com>
2020-06-21 14:06:27 -0500

    add jest env to eslint

--- server/modules/utilities.test.js
+++ server/modules/utilities.test.js
@@ -2,7 +2,6 @@ const { getDebugMessage } = require("../modules/utilities");
 
 describe("getDebugMessage() function", () => {
   test("If it is called without arguments it should return false", () => {
-    const testMessage = false;
     expect(getDebugMessage()).toBe(false);
   });
 });
