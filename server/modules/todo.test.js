7556fc20370c69384b04c23321e6469939230be9
Joseph Carey <josephcareycomposer@gmail.com>
2020-06-22 12:08:33 -0500

    add a basic test file for the todo file

---
+++ server/modules/todo.test.js
@@ -0,0 +1,22 @@
+ToDo = require("./todo");
+
+describe("getAllToDos():", () => {
+  test("true === true", () => {
+    expect(true).toBe(true);
+  });
+});
+describe("addToDo(toDoText):", () => {
+  test("true === true", () => {
+    expect(true).toBe(true);
+  });
+});
+describe("deleteToDo(idToDelete):", () => {
+  test("true === true", () => {
+    expect(true).toBe(true);
+  });
+});
+describe("editToDoText(idToEdit, newText):", () => {
+  test("true === true", () => {
+    expect(true).toBe(true);
+  });
+});
